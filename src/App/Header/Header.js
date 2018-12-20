0a7b14a3167b504847f50829e0505eff399cc680
theredwillow <theredwillows@gmail.com>
2018-12-20 12:35:37 -0600

    Started Header

---
+++ src/App/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import config from '../config';
+import './Header.css';
+import iconMap from '../../images/icon_map@2x.png';
+
+const Header = () => (
+    <div id="header" className="container-fluid">
+        <div id="header__app-name">{ config.appName }</div>
+        <div id="header__map-tile"><img src={iconMap} alt="Map"></img></div>
+    </div>
+);
+
+export default Header;
---
+++ src/App/config.js
@@ -0,0 +1,5 @@
+const config = {
+    appName: "Lunch Tyme"
+};
+
+export default config;
