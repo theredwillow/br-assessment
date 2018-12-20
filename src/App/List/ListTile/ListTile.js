0d94a53311569481401170b4599cd3a63893f94e
theredwillow <theredwillows@gmail.com>
2018-12-20 16:14:55 -0600

    Moved default into config

--- src/App/List/ListTile/ListTile.js
+++ src/App/List/ListTile/ListTile.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './ListTile.css';
 import hopdoddy from '../../../images/hopdoddy.png';
+import config from '../../config';
 
-const ListTile = ({name="Restaurant Name", category="Category Type", image=hopdoddy}) => {
+let placeholder = config.defaultRestaurant;
+
+const ListTile = ({
+    name=placeholder.name,
+    category=placeholder.category,
+    image=hopdoddy }) => {
     return (
-        <div className="list-tile" style={{ background: `url(${hopdoddy}) no-repeat center center`, backgroundSize: `100% auto` }}>
+        <div className="list-tile" style={{ background: `url(${image}) no-repeat center center`, backgroundSize: `100% auto` }}>
             <div className="list-tile__name">{ name }</div>
             <div className="list-tile__category">{ category }</div>
         </div>
--- src/App/config.js
+++ src/App/config.js
@@ -1,5 +1,15 @@
 const config = {
-    appName: "Lunch Tyme"
+    appName: "Lunch Tyme",
+    defaultRestaurant: {
+        name: "Restaurant Name",
+        category: "Category Type",
+        streetAddress: "5100 Belt Line Road, STE 502",
+        city: "Dallas",
+        state: "TX",
+        zip: "75206",
+        phone: "(972) 387-2337",
+        twitter: "twitterhandle"
+    }
 };
 
 export default config;
